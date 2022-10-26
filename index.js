398367e49197033961d6f3d462deef65ca9eaab1
Ahmad Musa <amaahmadmusa@gmail.com>
2022-10-25 22:47:50 -0700

    server is ready with basic api

--- index.js
+++ index.js
@@ -2,35 +2,12 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
-const allTopics = require('./data/all-topics.json')
-const topics = require('./data/topics.json')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/all-topics', (req, res) => {
-    res.send(allTopics)
-});
-
-app.get('/all-topics/:id', (req, res) => {
-    const id = req.params.id;
-        const selectedAllTopics = topics.find( n => n.allTopics_id === id)
-        res.send(selectedAllTopics)
-    })
-
-app.get('/topics', (req, res) =>{
-    res.send(topics);
-});
-
-app.get('/topics/:id', (req, res) => {
-    const id = req.params.id;
-        const selectedTopics = topics.find( n => n._id === id)
-        res.send(selectedTopics)
-})
+    res.send('Hello World!')
+  })
 
 
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
