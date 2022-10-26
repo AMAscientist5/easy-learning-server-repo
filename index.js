e2e7d630884d33b1d5659bc1cc71fc17924eb312
Ahmad Musa <amaahmadmusa@gmail.com>
2022-10-25 22:57:16 -0700

    other api is added

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
   })
 
+  app.get('/all-topics', (req, res) => {
+    res.send(allTopics)
+});
+
+app.get('/all-topics/:id', (req, res) => {
+    const id = req.params.id;
+        const selectedAllTopics = topics.find( n => n.allTopics_id === id)
+        res.send(selectedAllTopics)
+    })
+
+app.get('/topics', (req, res) =>{
+    res.send(topics);
+});
+
+app.get('/topics/:id', (req, res) => {
+    const id = req.params.id;
+        const selectedTopics = topics.find( n => n._id === id)
+        res.send(selectedTopics)
+})
+
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
