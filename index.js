e8ea83aeab6f548e7619a9083d45e93e4c9a9b59
Ahmad Musa <amaahmadmusa@gmail.com>
2022-10-25 22:54:14 -0700

    alltopics data is added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 
+const allTopics = require('./data/all-topics.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
